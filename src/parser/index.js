b3dd1b9d78c22ce4667688e8ef46e924fd964b20
Paweł Górski <dev.baymax42@gmail.com>
2018-12-22 20:42:13 +0100

    Documented parser functions.

--- src/parser/index.js
+++ src/parser/index.js
@@ -3,21 +3,53 @@ const RuleParser = require('./rule.jacobgram.out')
 const SystemLexer = require('./lsystem.jacoblex.out')
 const SystemParser = require('./lsystem.jacobgram.out')
 
+// Concrete parser and lexer for rules
 const ruleLexer = new RuleLexer()
 const ruleParser = new RuleParser()
 
+// Concrete parser and lexer for generating systems
 const systemLexer = new SystemLexer()
 const systemParser = new SystemParser()
 
+// Context produced by below functions is of form:
+//
+// context = {
+// <token>: [
+//      {
+//        params // parameters for this token
+//        create // function returning content of production rule
+//        check  // function checking if parameters are meeting condition
+//      }
+//    ],
+//    assemble   // function used at the end of parsing for assembling all results from 'create' functions
+//  }
+//
+//  For each token parser iterates through this context and checks
+//  if found token meets conditions: check function and number of parameters.
+//
+//  Similar context is used to transform result of parsing
+//  to more computer friendly format used by drawing system.
+//
+
+// Parse one rule string and return object with actions for given token
+//  {
+//    params: array    # parameters for this token
+//    create: function # function returning content of production rule
+//    check: function  # function checking if parameters are meeting condition
+//  }
 function digestRule (ruleStr) {
   let context = {
-    params: []
+    params: [],
+    create: null,
+    check: null
   }
   ruleLexer.setInput(ruleStr)
   let token = ruleParser.parse(ruleLexer, context)
   return [token, context]
 }
 
+// Parse array of rules
+//  array # array of parsed rules
 function transformRules (rules) {
   let parserContext = {}
   for (let rule of rules) {
@@ -27,21 +59,28 @@ function transformRules (rules) {
     }
     parserContext[result[0]].push(result[1])
   }
+  parserContext.assemble = (array) => {
+    return array.join('')
+  }
   return parserContext
 }
 
+// Make one iteration of word with set of rules
+//  string # result for given ruleset
 function iterateSystem (system, parserContext) {
   parserContext.params = []
   systemLexer.setInput(system)
   return systemParser.parse(systemLexer, parserContext)
 }
 
+// Iterate 'iterations' times generating system from given ruleset
 function evolve (axiom, transformedRules, iterations) {
   let system = axiom
   while (iterations-- > 0) {
     console.log(system)
     system = iterateSystem(system, transformedRules)
   }
+  console.log(system)
   return system
 }
 
