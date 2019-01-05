e0c806ccaa4d6917ac2fd949867167cc95572a2f
Ada Sozańska <ada.sozanska@gmail.com>
2019-01-05 21:39:37 +0100

    Fixed bug when turtle saved positions without cloning them and messed up everything :( Added function to interpret intermediary format for rendering.

--- src/turtle/index.js
+++ src/turtle/index.js
@@ -7,33 +7,34 @@ class Turtle {
     this.position = new THREE.Vector3(0, 0, 0)
     this.rotX = 0
     this.rotY = 0
-    this.rotZ = Math.PI / 2
+    this.rotZ = 0
     this.lines = []
     // additional params
   }
 
   moveTo (length) {
     let geometry = new THREE.Geometry()
-    geometry.vertices.push(this.position)
+    geometry.vertices.push(this.position.clone())
     geometry.vertices.push(new THREE.Vector3(this.position.x + length, this.position.y, this.position.z))
     geometry.rotateX(this.rotX)
     geometry.rotateY(this.rotY)
     geometry.rotateZ(this.rotZ)
     let line = new THREE.Line(geometry)
-    this.position = line.geometry.vertices[1]
+    this.position = line.geometry.vertices[1].clone()
   }
 
   drawTo (length) {
     let material = new THREE.LineBasicMaterial({color: 0x0000ff})
     let geometry = new THREE.Geometry()
-    geometry.vertices.push(this.position)
-    geometry.vertices.push(new THREE.Vector3(this.position.x + length, this.position.y, this.position.z))
+    geometry.vertices.push(new THREE.Vector3(0, 0, 0))
+    geometry.vertices.push(new THREE.Vector3(0, length, 0))
     geometry.rotateX(this.rotX)
     geometry.rotateY(this.rotY)
     geometry.rotateZ(this.rotZ)
+    geometry.translate(this.position.x, this.position.y, this.position.z)
     let line = new THREE.Line(geometry, material)
     this.lines.push(line)
-    this.position = line.geometry.vertices[1]
+    this.position = line.geometry.vertices[1].clone()
   }
 
   rotateX (angle) {
@@ -51,7 +52,7 @@ class Turtle {
   pushState () {
     this.stack.push({
       lineWidth: this.lineWidth,
-      position: this.position,
+      position: this.position.clone(),
       rotX: this.rotX,
       rotY: this.rotY,
       rotZ: this.rotZ
@@ -68,6 +69,29 @@ class Turtle {
   }
 }
 
+function drawSystem (actions) {
+  let turtle = new Turtle()
+  for (let action of actions) {
+    if (action[0] === 'DRAW') {
+      turtle.drawTo(action[1].length)
+    } else if (action[0] === 'MOVE') {
+      turtle.moveTo(action[1].length)
+    } else if (action[0] === 'ROTX') {
+      turtle.rotateX(action[1].angle)
+    } else if (action[0] === 'ROTY') {
+      turtle.rotateY(action[1].angle)
+    } else if (action[0] === 'ROTZ') {
+      turtle.rotateZ(action[1].angle)
+    } else if (action[0] === 'PUSH') {
+      turtle.pushState()
+    } else {
+      turtle.popState()
+    }
+  }
+  return turtle.lines
+}
+
 export default {
-  Turtle
+  Turtle,
+  drawSystem
 }
